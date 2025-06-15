3ba1dd9a71d31020205966576a86b05e11740b2e
Martin Wang <martinwangcs.dev@gmail.com>
2025-06-14 21:48:53 -0400

    Add pill in My Comments tab to show where the comment was scraped from for each comment format

--- frontend/market-knowledge-frontend/src/app/comments/page.tsx
+++ frontend/market-knowledge-frontend/src/app/comments/page.tsx
@@ -115,7 +115,16 @@ export default function Comments() {
                                 style={{ color: "#fff"}}>
                                 {comment.title}
                             </a>
-                            <span className="badge bg-primary rounded-pill">{comment.upvotes} upvotes</span>
+                            {/* <span className="badge bg-primary rounded-pill">
+                                {comment.upvotes} upvotes
+                                <span className="badge bg-secondary rounded-pill5 ms-2">
+                                    {comment.subreddit}
+                                </span>
+                            </span> */}
+                            <div>
+                                <span className="badge bg-primary rounded-pill">{comment.upvotes} upvotes</span>
+                                <span className="badge bg-secondary rounded-pill ms-2">{comment.subreddit}</span>
+                            </div>
                         </li>
                     ))}
                 </ul>
@@ -142,13 +151,18 @@ export default function Comments() {
                 </div>
                 <ul className="list-group">
                     {comments.map((comment) => (
+                        // Check if the body is not empty or just whitespace. If not, render the row.
                         comment.body && comment.body.trim() !== "" && (
                         <li 
                         key={comment.id} 
                         className="list-group-item"
                         style={{ backgroundColor: "#1E555C", color: "#fff", border: "none" }}>
                             {comment.body}
-                            <span className="badge bg-primary rounded-pill">{comment.upvotes} upvotes</span>
+                            {/* <span className="badge bg-primary rounded-pill">{comment.upvotes} upvotes</span> */}
+                            <div>
+                                <span className="badge bg-primary rounded-pill">{comment.upvotes} upvotes</span>
+                                <span className="badge bg-secondary rounded-pill ms-2">{comment.subreddit}</span>
+                            </div>
                         </li>
                     )
                     ))}
