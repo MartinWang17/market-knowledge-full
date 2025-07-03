1a9c97a49f0dbaefa1dbe803f5d940647d6c2bea
Martin Wang <martinwangcs.dev@gmail.com>
2025-07-03 13:51:41 -0400

    Re render the comments after changes have been made to the collections. (Also included a CommentSection component which will be responsible for rendering all comments in the future. Not top priority right now to optimize)

--- frontend/market-knowledge-frontend/src/app/collections/[slug]/page.tsx
+++ frontend/market-knowledge-frontend/src/app/collections/[slug]/page.tsx
@@ -1,35 +1,37 @@
 "use client";
 import { useParams } from 'next/navigation';
-import { FaExternalLinkAlt } from 'react-icons/fa';
 import { useEffect, useState } from 'react';
 import { Comment } from '../../comments/types';
 import RenderFormatSelector from '../../formatSelector';
 import LoadingSpinner from '../../loadingSpinner'
-import { CardCommentList, TitleCommentList, BodyCommentList } from '../../comments/components'
 import GetCollections from '../../getCollections'
+import CommentsManager from '../../comments/components/CommentsManager'
+import RenderCollectionModal from '../../comments/components/collectionModal'
 
 export default function CollectionSlugPage() {
 
     const [comments, setComments] = useState<Comment[]>([]);
     const [commentFormat, setCommentFormat] = useState("card"); // "card", "title", "body"
     const [commentFilter, setCommentFilter] = useState("relevance"); // "all", "upvoted", "downvoted", etc.
+    const [showCollectionModal, setShowCollectionModal] = useState(false);
+    const [activePost, setActivePost] = useState<Comment | null>(null);
     const [loading, setLoading] = useState(true);
     const { collections } = GetCollections();
 
-        useEffect(() => {
-        fetch(`http://localhost:8000/comments?filter=${commentFilter}`)
-            .then(res => res.json())
-            .then(data => {
-                // data.comments is the array of posts from Supabase
-                console.log("Fetched comments:", data.comments);
-                setComments(data.comments);
-                setLoading(false);
-            })
-            .catch(error => {
-                console.error("Error fetching comments:", error);
-                setLoading(false);
-            });
-    }, [commentFilter]) //Refresh comments when filter changes
+    useEffect(() => {
+    fetch(`http://localhost:8000/comments?filter=${commentFilter}`)
+        .then(res => res.json())
+        .then(data => {
+            // data.comments is the array of posts from Supabase
+            console.log("Fetched comments:", data.comments);
+            setComments(data.comments);
+            setLoading(false);
+        })
+        .catch(error => {
+            console.error("Error fetching comments:", error);
+            setLoading(false);
+        });
+    }, [commentFilter, showCollectionModal]) //Refresh comments when filter changes
 
     const deleteComment = async (id: string) => {
     try {
@@ -52,7 +54,7 @@ export default function CollectionSlugPage() {
     const params = useParams();
     const slug = params.slug as string;
 
-    //update comment to only include comments in the current collection
+    //update comment to only include comments in the current collection. Checks if the comment has a collection and if it includes the current collection name
     const collectionComments = comments.filter((comment) => comment.collections && comment.collections.includes(slug));
 
     if (loading) {
@@ -70,9 +72,22 @@ export default function CollectionSlugPage() {
                 commentFilter={commentFilter}
                 setCommentFilter={setCommentFilter}
             />
-        {commentFormat === "card" && <CardCommentList comments={collectionComments} onDelete={deleteComment} collections={collections}/>}
-        {commentFormat === "body" && <BodyCommentList comments={collectionComments} onDelete={deleteComment} collections={collections}/>}
-        {commentFormat === "title" && <TitleCommentList comments={collectionComments} onDelete={deleteComment} collections={collections}/>}
+            <CommentsManager 
+                comments={collectionComments}
+                onDelete={deleteComment}
+                collections={collections} 
+                commentFormat={commentFormat}
+                showCollectionModal={showCollectionModal}
+                setShowCollectionModal={setShowCollectionModal}
+                activePost={activePost}
+                setActivePost={setActivePost}
+                />
+            <RenderCollectionModal 
+                showCollectionModal={showCollectionModal}
+                setShowCollectionModal={setShowCollectionModal}
+                post={activePost}
+                collections={collections}
+            />
     </div>
     );
 }
---
+++ frontend/market-knowledge-frontend/src/app/comments/components/CommentSection.tsx
@@ -0,0 +1,88 @@
+"use client";
+import { useEffect, useState } from 'react';
+import { Comment } from '../types';
+import RenderFormatSelector from '../../formatSelector';
+import LoadingSpinner from '../../loadingSpinner'
+import GetCollections from '../../getCollections'
+import CommentsManager from './CommentsManager'
+import RenderCollectionModal from './collectionModal'
+
+export default function CommentSection() {
+const [comments, setComments] = useState<Comment[]>([]);
+    const [commentFormat, setCommentFormat] = useState("card"); // "card", "title", "body"
+    const [commentFilter, setCommentFilter] = useState("relevance"); //
+    const [showCollectionModal, setShowCollectionModal] = useState(false);
+    const [activePost, setActivePost] = useState<Comment | null>(null);
+    const [loading, setLoading] = useState(true);
+    const { collections } = GetCollections();
+
+    useEffect(() => {
+    fetch(`http://localhost:8000/comments?filter=${commentFilter}`)
+        .then(res => res.json())
+        .then(data => {
+            // data.comments is the array of posts from Supabase
+            console.log("Fetched comments:", data.comments);
+            setComments(data.comments);
+            setLoading(false);
+        })
+        .catch(error => {
+            console.error("Error fetching comments:", error);
+            setLoading(false);
+        });
+    }, [commentFilter]) //Refresh comments when filter changes
+
+    // delete comment function
+    const deleteComment = async (id: string) => {
+        try {
+            const response = await fetch('http://localhost:8000/comments/' + id, {
+                method: 'DELETE',
+            });
+            if (response.ok) {
+                // Remove the deleted comment from local state
+                setComments(prev => prev.filter(comment => comment.id !== id));
+            } 
+            else {
+                const data = await response.json();
+                alert("Error deleting comment: " + (data.error || response.status));
+            }
+        }   catch (error) {
+            alert("Network error deleting comment.")
+        }
+    };
+
+    if (loading) {
+            return <LoadingSpinner />
+        }
+    
+    if (comments.length === 0) {
+        return (
+            <div className="text-center">No comments...yet. <br/>Start a scrape and start knowing your market!</div>
+        );
+    }
+
+    return (
+            <div className="container py-4">
+                <RenderFormatSelector 
+                    commentFormat={commentFormat}
+                    setCommentFormat={setCommentFormat}
+                    commentFilter={commentFilter}
+                    setCommentFilter={setCommentFilter}
+                    />
+                <CommentsManager 
+                    comments={comments}
+                    onDelete={deleteComment}
+                    collections={collections} 
+                    commentFormat={commentFormat}
+                    showCollectionModal={showCollectionModal}
+                    setShowCollectionModal={setShowCollectionModal}
+                    activePost={activePost}
+                    setActivePost={setActivePost}
+                    />
+                <RenderCollectionModal 
+                    showCollectionModal={showCollectionModal}
+                    setShowCollectionModal={setShowCollectionModal}
+                    post={activePost}
+                    collections={collections}
+                />
+            </div>
+)};
