b95c7e38ae6f1cf4ed823eba82a775853f8437f2
Martin Wang <martinwangcs.dev@gmail.com>
2025-07-23 20:42:32 -0400

    Make the features page responsive

--- frontend/market-knowledge-frontend/src/app/features/page.tsx
+++ frontend/market-knowledge-frontend/src/app/features/page.tsx
@@ -6,19 +6,22 @@ export default function FeaturesPage () {
 
     return (
         <>
+        
         <div className="hero-wrapper" style={{ marginBottom: "2px"}}>
-            <div id="hero" className="blurred-container" style={{ marginTop: "32px", marginBottom: "2rem"}}>
-                <div>
-                    <h1 className="main-heading">
-                    Get to know your market on a <span className="green-font">personal level</span> instantly
-                    </h1>
-                    <p className="lead">
-                        Save hours of your time by collecting thousands of relevant conversations within seconds, and extract invaluable information about your customers!
-                    </p>
+            <div className="row row-cols-12">
+                <div id="hero" className="blurred-container p-4 p-md-5" style={{ marginTop: "32px", marginBottom: "2rem"}}>
+                    <div>
+                        <h1 className="main-heading fs-2 fs-md-1 mb-3">
+                        Get to know your market on a <span className="green-font">personal level</span> instantly
+                        </h1>
+                        <p className="lead">
+                            Save hours of your time by collecting thousands of relevant conversations within seconds, and extract invaluable information about your customers!
+                        </p>
+                    </div>
                 </div>
             </div>
             <div id="start-scraping" 
-                className="d-flex flex-column justify-content-between"
+                className="d-flex flex-column justify-content-between row"
                 style={{ alignSelf: "center", marginTop: "10rem", marginBottom: "5rem", zIndex: 2 }}>
                 <h1 className="main-heading">Know your market now!</h1>
                 <Link href="/" className="btn btn-brand btn-lg">
@@ -33,14 +36,15 @@ export default function FeaturesPage () {
                 </span>
                 </Link>
             </div>
-            <div id="how-it-works" style={{ maxWidth: "90vw", textAlign: "start", marginBottom: "4rem", marginLeft: "10vw"}}>
+            <div id="how-it-works">
                 <span className="blue-light"></span>
                 <h1 className="main-heading justify-content-start" style={{ marginTop: "5rem" }}>
                     How it <br/><span className="blue-font">Works?</span>
                 </h1>
-                <div className="hiw-container">
+                <div className="hiw-container justify-content-center row row-cols-1 row-cols-md-3 g-4">
                     <div className="hiw-card">
                         <div>
+                            <i className="bi bi-search" style={{ fontSize: "2.5rem", marginBottom: "1rem", color: "#285280" }} />
                             <h2 style={{ zIndex: 3 }}>
                                 Search
                             </h2>
@@ -51,6 +55,7 @@ export default function FeaturesPage () {
                     </div>
                     <div className="hiw-card">
                         <div>
+                            <i className="bi bi-database" style={{ fontSize: "2.5rem", marginBottom: "1rem", color: "#285280" }}></i>
                             <h2>
                                 Scrape
                             </h2>
@@ -61,6 +66,7 @@ export default function FeaturesPage () {
                     </div>
                     <div className="hiw-card">
                         <div>
+                            <i className="bi bi-file-earmark-arrow-down" style={{ fontSize: "2.5rem", marginBottom: "1rem", color: "#285280" }}></i>
                             <h2>
                                 Export
                             </h2>
