23adec54fe1b5ea7acf42b6810dccadbe0076dda
Martin Wang <martinwangcs.dev@gmail.com>
2025-07-31 12:42:31 -0400

    Change favicon to Market Knowledge logo

--- frontend/market-knowledge-frontend/src/app/layout.tsx
+++ frontend/market-knowledge-frontend/src/app/layout.tsx
@@ -30,6 +30,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+      </head>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <Navbar />
         <div
